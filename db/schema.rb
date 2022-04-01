438d9027e4571971df217ecb76716e77b03af5b8
EC2 Default User <ec2-user@ip-172-31-14-39.ap-northeast-1.compute.internal>
2022-04-01 17:09:26 +0000

    update README

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_01_071213) do
+ActiveRecord::Schema.define(version: 2022_04_01_170345) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
